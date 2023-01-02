e65730970ef080f5b2f858012e3ca369628fca08
arreme <a.reme@hotmail.com>
2023-01-02 13:21:37 +0100

    Helicopter Basic Movement

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Helicoopter
+{
+    [RequireComponent(typeof(HelicopterController))]
+    public class PlayerInput : MonoBehaviour
+    {
+        private PlayerInputScheme _scheme;
+        private HelicopterController _helicopterController;
+
+        private void Awake()
+        {
+            _helicopterController = GetComponent<HelicopterController>();
+            _scheme = new PlayerInputScheme();
+            _scheme.Player.Movement.performed += ctx => LeftRight(ctx.ReadValue<float>());
+            _scheme.Player.Engine.performed += ctx => TurnEngine(ctx.ReadValue<float>());
+        }
+
+        private void TurnEngine(float engine)
+        {
+            _helicopterController.SetEngine(Mathf.Approximately(engine,1));
+        }
+
+        private void LeftRight(float input)
+        {
+            _helicopterController.SetMovement(input);
+        }
+
+        private void OnEnable()
+        {
+            _scheme.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _scheme.Disable();
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/HelicopterController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Helicoopter
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class HelicopterController : MonoBehaviour
+    {
+        private bool _inputEngine;
+        private float _inputMovement;
+
+        private Rigidbody2D _rb;
+
+        [SerializeField] private float sGravity;
+        
+        [Header("Up Settings")]
+        [SerializeField] private float sMaxAcc;
+        [SerializeField] private float sMaxAccTime;
+        [SerializeField] private AnimationCurve sUpSpeedCurve;
+
+        private float _currentAcc = 0;
+
+        [Header("Turn Settings")]
+        [SerializeField] private float sInputRate;
+        [SerializeField] private float sMaxDegrees;
+        private float _turnSpeed;
+        private float _direction;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            //Up and Down
+            _currentAcc = Mathf.Clamp(_currentAcc + (Time.deltaTime * (_inputEngine ? 1 : -1)),0,sMaxAccTime);
+            float acc = sUpSpeedCurve.Evaluate(_currentAcc) * sMaxAcc;
+            _rb.AddForce(_rb.mass * acc * transform.up,ForceMode2D.Force);
+            _rb.AddForce(sGravity * Vector2.down, ForceMode2D.Force);
+            
+            //Movement
+            _direction = Mathf.SmoothDamp(_direction,_inputMovement * acc,ref _turnSpeed,sInputRate);
+            transform.eulerAngles = _direction * sMaxDegrees * Vector3.back;
+        }
+
+        public void SetEngine(bool engine)
+        {
+            _inputEngine = engine;
+        }
+
+        public void SetMovement(float movement)
+        {
+            _inputMovement = movement;
+        }
+    }
+}
